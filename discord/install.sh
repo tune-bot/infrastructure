cf3b731e66e3ef4e93f5537bf74666b14e882213
cbeimers113 <chrisbeimers113@gmail.com>
2023-07-24 12:40:22 -0400

    Remove bot token prompt from Discord install

--- discord/install.sh
+++ discord/install.sh
@@ -3,11 +3,6 @@
 rm -rf discord
 git clone https://github.com/tune-bot/discord.git
 
-if grep -q BOT_TOKEN infrastructure/discord/vars.env; then
-    read -p "Enter Discord Bot Token: " BOT_TOKEN
-    sed -i "s|BOT_TOKEN|$BOT_TOKEN|g" infrastructure/discord/vars.env
-fi
-
 mkdir -p bin
 echo "#!/bin/bash" > bin/discord
 echo "bash infrastructure/util/golang.sh" >> bin/discord
